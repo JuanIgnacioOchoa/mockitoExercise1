ea4b39b70e5a504c651b195808552f71cedc0769
JuanIgnacioOchoa <juanignacio8ag@gmail.com>
2015-09-13 12:46:43 -0500

    MockitoExercise

---
+++ mockitoExercise/src/main/test/java/mx/iteso/TacitKnowledgePrinterTest.java
@@ -0,0 +1,23 @@
+package mx.iteso;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class TacitKnowledgePrinterTest {
+    TacitKnowledgePrinter tkñ
+    //Test printNumbers with a limit of 10
+
+
+    //Test printNumbers with a limit of 0
+        //Verify Exception
+
+    //What else can we test?
+        //What should we verify?
+
+    @Test
+    public void forbiddenTest() {
+        assertTrue(true);
+    }
+
+}
